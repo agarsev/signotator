19ee4d47b9eca96f77c65ebd5e897ab938128cf6
Antonio F. G. Sevilla <antonio@garciasevilla.com>
2022-09-16 21:00:25 +0200

    options tab, signer/observer perspective option for dirs

--- main.js
+++ main.js
@@ -7,11 +7,13 @@ import { O } from './space.js';
 const tabs = {
     Q: { component: Q, next: "O" },
     O: { component: O, next: "Q" },
+    "⚙": { component: Options, next: "Q" },
 };
 
 const fixColons = /:(?=:)|^:|:$/g;
 
 export default function Signotator ({ inputRef, updateVal }) {
+    const [options, setOptions] = useLocalStorage("signotator-opts", DEF_OPTIONS);
     const [tab, setTab] = useState("Q");
     const Component = tabs[tab].component;
     const appendSN = SN => {
@@ -35,6 +37,40 @@ export default function Signotator ({ inputRef, updateVal }) {
         <nav>{Object.keys(tabs).map(seg=> <button key={seg}
                 disabled={tab==seg} onClick={() => setTab(seg)}>
             {seg}</button>)}</nav>
-        <Component done={appendSN} />
+        <Component done={appendSN} options={options} setOptions={setOptions} />
     </div>;
 }
+
+function Options ({ options, setOptions }) {
+
+    function set(pref, val) { setOptions({ ...options, [pref]: val }); }
+
+    function Radio({ text, pref, val }) {
+        return <label onClick={() => set(pref, val)}>
+            <input type="radio" name={pref} value={val} onChange={e => set(pref, e.target.value)}
+                checked={options[pref]==val} /> {text}</label>;
+    }
+
+    return <div>
+        Perspectiva:
+        <Radio pref="perspective" text="Observador" val="obs" />
+        <Radio pref="perspective" text="Signante" val="sign" />
+    </div>;
+}
+
+const DEF_OPTIONS = {
+    perspective: "sign"
+};
+
+function useLocalStorage(key, def) {
+    let stored;
+    try {
+        stored = JSON.parse(localStorage.getItem(key));
+    } catch {} 
+    const [ val, set ] = useState(stored!==undefined?stored:def);
+    return [ val, new_val => {
+        localStorage.setItem(key, JSON.stringify(new_val));
+        set(new_val);
+    }];
+}
+
--- space.js
+++ space.js
@@ -1,7 +1,12 @@
 import { useState } from "react";
 
-export function Direction ({ val, set }) {
-    function Arrow({ dir, opo, path }) {
+export function Direction ({ val, set, invert }) {
+    function Arrow({ dir, opo, inv, path }) {
+        if (inv) {
+            let x = dir;
+            dir = opo;
+            opo = x;
+        }
         let cn = "Arrow";
         let click = null;
         if (val.includes(dir)) {
@@ -16,25 +21,25 @@ export function Direction ({ val, set }) {
         return <path d={path} className={cn} onClick={click} />;
     }
     return <svg className="w-full h-full" viewBox="90 52 62 58">
-        <Arrow dir="F" opo="B" path="m 133.66599,62.189793 -16.63545,6.86199 8.18301,0.0785 -5.84265,8.538184 7.35789,0.07058 5.84265,-8.538184 8.18299,0.0785 z" />
-        <Arrow dir="H" opo="L" path="m 120.15121,52.244762 -9.71724,10.715056 h 6.70347 v 13.114959 h 6.02754 V 62.959818 h 6.70346 z" />
-        <Arrow dir="X" opo="Y" path="m 91.006791,81.042921 5.856436,7.820849 3.351733,-5.395239 h 13.11496 l 3.01377,-4.851221 h -13.11496 l 3.35173,-5.395231 z" />
-        <Arrow dir="Y" opo="X" path="m 150.49164,81.125927 -5.85643,-7.820849 -3.35174,5.395239 h -13.11496 l -3.01377,4.851221 h 13.11496 l -3.35173,5.395231 z" />
-        <Arrow dir="L" opo="H" path="m 121.34719,109.92412 9.71724,-10.715056 h -6.70347 V 86.094105 h -6.02754 v 13.114959 h -6.70346 z" />
-        <Arrow dir="B" opo="F" path="m 107.83243,99.97909 16.63545,-6.86199 -8.18301,-0.0785 5.84265,-8.538184 -7.35789,-0.07059 -5.84264,8.538184 -8.183,-0.0785 z" />
+        <Arrow dir="F" opo="B" inv={invert} path="m 133.66599,62.189793 -16.63545,6.86199 8.18301,0.0785 -5.84265,8.538184 7.35789,0.07058 5.84265,-8.538184 8.18299,0.0785 z" />
+        <Arrow dir="H" opo="L" inv={false}  path="m 120.15121,52.244762 -9.71724,10.715056 h 6.70347 v 13.114959 h 6.02754 V 62.959818 h 6.70346 z" />
+        <Arrow dir="X" opo="Y" inv={invert} path="m 91.006791,81.042921 5.856436,7.820849 3.351733,-5.395239 h 13.11496 l 3.01377,-4.851221 h -13.11496 l 3.35173,-5.395231 z" />
+        <Arrow dir="Y" opo="X" inv={invert} path="m 150.49164,81.125927 -5.85643,-7.820849 -3.35174,5.395239 h -13.11496 l -3.01377,4.851221 h 13.11496 l -3.35173,5.395231 z" />
+        <Arrow dir="L" opo="H" inv={false}  path="m 121.34719,109.92412 9.71724,-10.715056 h -6.70347 V 86.094105 h -6.02754 v 13.114959 h -6.70346 z" />
+        <Arrow dir="B" opo="F" inv={invert} path="m 107.83243,99.97909 16.63545,-6.86199 -8.18301,-0.0785 5.84265,-8.538184 -7.35789,-0.07059 -5.84264,8.538184 -8.183,-0.0785 z" />
     </svg>;
 }
 
-export function O ({ done }) {
+export function O ({ done, options }) {
     const [palmar, setPalmar] = useState([]);
     const [distal, setDistal] = useState([]);
     return <div><table>
         <tbody>
             <tr><th>Palma</th><th>Dedos</th></tr>
             <tr><td>
-                <Direction val={palmar} set={setPalmar} />
+                <Direction val={palmar} set={setPalmar} invert={options.perspective=="obs"} />
             </td><td>
-                <Direction val={distal} set={setDistal} />
+                <Direction val={distal} set={setDistal} invert={options.perspective=="obs"} />
             </td></tr>
             <tr><td colSpan="2" className="text-right"><button className="finish"
                 disabled={palmar.length==0 && distal.length==0}
