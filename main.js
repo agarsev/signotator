02d6136d96175f43aa40b11b2a7d6fb995e73199
Antonio F. G. Sevilla <antonio@garciasevilla.com>
2022-10-03 19:07:31 +0200

    Fix bug with preferences not yet set

--- main.js
+++ main.js
@@ -68,8 +68,9 @@ function useLocalStorage(key, def) {
     let stored;
     try {
         stored = JSON.parse(localStorage.getItem(key));
-    } catch {} 
-    const [ val, set ] = useState(stored!==undefined?stored:def);
+    } catch {}
+    if (stored===undefined || stored===null) stored = def;
+    const [ val, set ] = useState(stored);
     return [ val, new_val => {
         localStorage.setItem(key, JSON.stringify(new_val));
         set(new_val);
