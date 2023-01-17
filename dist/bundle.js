dc698f66e5da9bed18e12830e89464a1ff55bc52
Antonio F. G. Sevilla <antonio@garciasevilla.com>
2023-01-17 17:39:11 +0100

    Mejoras input signotation

--- dist/bundle.js
+++ dist/bundle.js
@@ -742,16 +742,17 @@ function useLocalStorage(key, def) {
 
 // src/input.js
 import { useRef } from "react";
-function SignotationInput({ value, inputRef, updateVal }) {
+function SignotationInput({ value, updateVal, inputName = "", inputRef }) {
   const hili = useRef();
   return <div className="relative">
-    <div ref={hili} className="p-1 w-full pointer-events-none bg-white rounded whitespace-nowrap overflow-hidden"><Signotation sn={value || "\xA0"} /></div>
+    <div ref={hili} className="py-1 px-2 w-full pointer-events-none bg-white rounded whitespace-nowrap overflow-hidden"><Signotation sn={value || "\xA0"} /></div>
     <input
-      className="p-1 w-full font-mono absolute top-0 bg-transparent rounded border border-signotatormain-600 outline-none"
+      className="py-1 px-2 w-full font-mono absolute top-0 bg-transparent rounded border border-signotatormain-600 outline-none"
       type="text"
       style={{ color: "transparent", caretColor: "black" }}
       value={value || ""}
       ref={inputRef}
+      name={inputName}
       onScroll={(e) => {
         hili.current.scrollLeft = e.target.scrollLeft;
       }}
